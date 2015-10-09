95858c0c810fb23125fde4340d41c596fc997970
Jack Eller <jheguy2@gmail.com>
2015-10-09 10:58:31 -0400

    Begins bootstrap development

--- index.php
+++ index.php
@@ -1,20 +1,24 @@
 <?php
 $version = 0.1;
 ?>
-
-<html>
+<html lang="en">
 
 <head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
 <title>
 Steam Site
 </title>
 <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" type="text/css" href="style.css">
+<link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 
 <body>
-
+<script src="libs/jquery-2.1.4.min.js">
+<script src="js/bootstrap.min.js"></script>
 <h1>
 testing fonts, and git deployment site version is:
 <?php
