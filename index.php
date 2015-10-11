deaa65ccf9926208f6530170c830ae618090e039
Jack Eller <jheguy2@gmail.com>
2015-10-11 15:56:13 -0400

    Adds basic bootstrap framework, with menu and page content

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$version = 0.1;
+$version = 0.2;
 ?>
 <html lang="en">
 
@@ -13,20 +13,40 @@ Steam Site
 </title>
 <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 <link href="css/bootstrap.min.css" rel="stylesheet">
+script src="libs/jquery-2.1.4.min.js">
+<script src="js/bootstrap.min.js"></script>
 </head>
 
 
 <body>
-<script src="libs/jquery-2.1.4.min.js">
-<script src="js/bootstrap.min.js"></script>
-<div align="center">
-<h1>
-testing fonts, and git deployment site version is:
-<?php
-echo $version;
-?>
-</h1>
-</div>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">SteamSlap</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#faq">FAQ</a></li>
+            <li><a href="#login">Login</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
 
+    <div class="container">
+
+      <div class="starter-template">
+        <h1>Welcome to SteamSlap!</h1>
+        <p class="lead">We are currently in development,<br> as of now we are at version: <?php echo $version ?></p>
+      </div>
+
+    </div>
 </body>
 </html>
